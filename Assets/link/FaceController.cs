5b7a11644f6ec3b6d3d55ec1be0cecdf3afa5bf1
charles <netman.cn@gmail.com>
2017-04-07 15:10:06 +0800

    优化瞳孔控制

--- Assets/link/FaceController.cs
+++ Assets/link/FaceController.cs
@@ -23,9 +23,15 @@ public class FaceController : MonoBehaviour
     private Material _eyebrowMtl1 = null;
     private Material _eyebrowMtl2 = null;
 
-    private int eyeIndex = 0;
-    private int eyeBrowIndex = 0;
-    private int _maskIndex = 0;
+    private int _eyeIndex = 0;
+    private int _eyeBrowIndex = 0;
+    private int _maskId = 0;
+    private float _pupilX = 0;
+    private float _pupilY = 0;
+    private float _pupilS = 1;
+    private int _XOffsetId = 0;
+    private int _YOffsetId = 0;
+    private int _ScaleId = 0;
 
     // Use this for initialization
     void Start ()
@@ -38,14 +44,11 @@ public class FaceController : MonoBehaviour
         _eyebrowMtl2 = eyebrowObj2.GetComponent<MeshRenderer>().material;
         _pupilMtl1 = pupilObj1.GetComponent<MeshRenderer>().material;
         _pupilMtl2 = pupilObj2.GetComponent<MeshRenderer>().material;
-        _maskIndex = Shader.PropertyToID("_EyeMaskTex");
+        _maskId = Shader.PropertyToID("_EyeMaskTex");
+        _XOffsetId = Shader.PropertyToID("_XOffset");
+        _YOffsetId = Shader.PropertyToID("_YOffset");
+        _ScaleId = Shader.PropertyToID("_Scale");
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-	
-	}
 
     private void OnGUI()
     {
@@ -58,20 +61,46 @@ public class FaceController : MonoBehaviour
 
         if (GUILayout.Button("更换Eye"))
         {
-            eyeIndex++;
-            eyeIndex = eyeIndex % eyes.Length;
-            _eyeMtl1.SetTexture(_mainTexId, eyes[eyeIndex]);
-            _eyeMtl2.SetTexture(_mainTexId, eyes[eyeIndex]);
-            _pupilMtl1.SetTexture(_maskIndex, eyes[eyeIndex]);
-            _pupilMtl2.SetTexture(_maskIndex, eyes[eyeIndex]);
+            _eyeIndex++;
+            _eyeIndex = _eyeIndex % eyes.Length;
+            _eyeMtl1.SetTexture(_mainTexId, eyes[_eyeIndex]);
+            _eyeMtl2.SetTexture(_mainTexId, eyes[_eyeIndex]);
+            _pupilMtl1.SetTexture(_maskId, eyes[_eyeIndex]);
+            _pupilMtl2.SetTexture(_maskId, eyes[_eyeIndex]);
         }
 
         if (GUILayout.Button("更换Eyebrow"))
         {
-            eyeBrowIndex++;
-            eyeBrowIndex = eyeBrowIndex % eyebrows.Length;
-            _eyebrowMtl1.SetTexture(_mainTexId, eyebrows[eyeBrowIndex]);
-            _eyebrowMtl2.SetTexture(_mainTexId, eyebrows[eyeBrowIndex]);
+            _eyeBrowIndex++;
+            _eyeBrowIndex = _eyeBrowIndex % eyebrows.Length;
+            _eyebrowMtl1.SetTexture(_mainTexId, eyebrows[_eyeBrowIndex]);
+            _eyebrowMtl2.SetTexture(_mainTexId, eyebrows[_eyeBrowIndex]);
+        }
+
+        GUILayout.Label("瞳孔位置X");
+        float pupilX = GUILayout.HorizontalSlider(_pupilX, -0.5f, 0.5f);
+        if (pupilX != _pupilX)
+        {
+            _pupilX = pupilX;
+            _pupilMtl1.SetFloat(_XOffsetId, _pupilX);
+            _pupilMtl2.SetFloat(_XOffsetId, _pupilX);
+        }
+       
+        GUILayout.Label("瞳孔位置Y");
+        float pupilY = GUILayout.HorizontalSlider(_pupilY, -0.5f, 0.5f);
+        if (pupilY != _pupilY)
+        {
+            _pupilY = pupilY;
+            _pupilMtl1.SetFloat(_YOffsetId, _pupilY);
+            _pupilMtl2.SetFloat(_YOffsetId, _pupilY);
+        }
+        GUILayout.Label("瞳孔放大");
+        float pupilS = GUILayout.HorizontalSlider(_pupilS, 0, 2);
+        if (pupilS != _pupilS)
+        {
+            _pupilS = pupilS;
+            _pupilMtl1.SetFloat(_ScaleId, 2 - _pupilS);
+            _pupilMtl2.SetFloat(_ScaleId, 2 - _pupilS);
         }
     }
 }
